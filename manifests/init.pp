4cc3050947b5ead48517b190f55dcdae67ebd4de
Les Aker <me@lesaker.org>
2022-08-07 13:49:05 -0400

    add udev config

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,11 +1,33 @@
 # @summary Configure UPS monitoring
 #
-class ups {
+# @param vendorid sets the USB Vendor ID for the UPS
+# @param productid sets the USB Product ID for the UPS
+class ups (
+  String $vendorid,
+  String $productid,
+) {
   package { 'nut': }
 
+  -> file { '/etc/udev/rules.d/50-ups.rules':
+    ensure  => file,
+    content => template('ups/udev.rules.erb'),
+  }
+
+  ~> exec { 'reload udev':
+    command => '/usr/bin/udevadm control --reload-rules',
+    path    => '/usr/bin',
+    refreshonly => true,
+  }
+
+  ~> exec { 'retrigger udev':
+    command     => '/usr/bin/udevadm trigger',
+    path        => '/usr/bin',
+    refreshonly => true,
+  }
+
   -> file { '/etc/nut/ups.conf':
-    ensure => file,
-    source => 'puppet:///modules/ups/ups.conf',
+    ensure  => file,
+    content => template('ups/ups.conf.erb'),
   }
 
   ~> service { 'nut-driver@ups':
